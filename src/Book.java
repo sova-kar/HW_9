6c7e7f5c7d725429aedb5d62ec058d38d5f94a09
SOVA <sova16tais@gmail.com>
2024-12-07 21:37:38 +0300

    Did my homework_9_part2

--- src/Book.java
+++ src/Book.java
@@ -1,3 +1,5 @@
+import java.util.Objects;
+
 public class Book {
 
     private String title;
@@ -24,5 +26,27 @@ public class Book {
 
     public void setPublishingYear(Integer publishingYear) {
         this.publishingYear = publishingYear;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Book book = (Book) o;
+        return Objects.equals(title, book.title) && Objects.equals(author, book.author) && Objects.equals(publishingYear, book.publishingYear);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(title, author, publishingYear);
+    }
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "title='" + title + '\'' +
+                ", author=" + author +
+                ", publishingYear=" + publishingYear +
+                '}';
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -4,14 +4,23 @@ public class Main {
         Author remark = new Author("Эрих Мария", "Ремарк");
 
         Book theSorrows = new Book("Скорбь Сатаны", korelli,1895);
+        Book theSorrows2 = new Book("Скорбь Сатаны", korelli,1895);
         Book arcDeTriomphe = new Book("Триумфальная арка", remark,1939);
 
-        System.out.println(theSorrows.getTitle()+ " " + korelli.getFirstName()+ " " + korelli.getSecondName()+ " " + theSorrows.getPublishingYear());
-        System.out.println(arcDeTriomphe.getTitle()+ " " + remark.getFirstName()+ " " + remark.getSecondName()+ " " + arcDeTriomphe.getPublishingYear());
+        System.out.println(theSorrows);
+        System.out.println(arcDeTriomphe);
+
+//        System.out.println(theSorrows.getTitle()+ " " + korelli.getFirstName()+ " " + korelli.getSecondName()+ " " + theSorrows.getPublishingYear());
+//        System.out.println(arcDeTriomphe.getTitle()+ " " + remark.getFirstName()+ " " + remark.getSecondName()+ " " + arcDeTriomphe.getPublishingYear());
+
+        System.out.println("Сравнение theSorrows " + theSorrows.equals(theSorrows2));
 
         theSorrows.setPublishingYear(2024);
+        System.out.println(theSorrows);
 
-        System.out.println(theSorrows.getTitle() + " " + korelli.getFirstName()+ " " + korelli.getSecondName() + " " + theSorrows.getPublishingYear());
+//        System.out.println(theSorrows.getTitle() + " " + korelli.getFirstName()+ " " + korelli.getSecondName() + " " + theSorrows.getPublishingYear());
+
+        System.out.println("Сравнение theSorrows " + theSorrows.equals(theSorrows2));
 
 
     }
